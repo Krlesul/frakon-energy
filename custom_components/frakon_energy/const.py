ac779d49564e11db3c2efbd31b74b081f5eef34d
Krlesul <132875575+Krlesul@users.noreply.github.com>
2026-08-04 12:45:46 +0200

    feat: align VisionQ constants with official API

--- custom_components/frakon_energy/const.py
+++ custom_components/frakon_energy/const.py
@@ -1,14 +1,14 @@
 from __future__ import annotations
 
 DOMAIN = "frakon_energy"
-
 PLATFORMS: tuple[str, ...] = ("sensor",)
 
 CONF_PROVIDER = "provider"
 CONF_SCAN_INTERVAL = "scan_interval"
+CONF_EUI = "eui"
 
 PROVIDER_VISIONQ = "visionq"
 
-DEFAULT_SCAN_INTERVAL = 300
-MIN_SCAN_INTERVAL = 60
-MAX_SCAN_INTERVAL = 3600
+DEFAULT_SCAN_INTERVAL = 1800
+MIN_SCAN_INTERVAL = 900
+MAX_SCAN_INTERVAL = 86400
